496530f1e656a0e84ae4b79da06346155e6a372a
Muharrem Emin <144062877+Kzuyaa@users.noreply.github.com>
2025-08-27 09:32:14 +0300

    Delete build directory

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DigitalClock_FILE C:/Qtproject/DigitalClock/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/DigitalClock.exe)
-set(__QT_DEPLOY_TARGET_DigitalClock_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DigitalClock_RUNTIME_DLLS D:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
